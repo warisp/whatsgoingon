abf53f2ca0631cf294faf3c9fa0f31111dc3a7bf
WP <warispaktian@Live.com>
2020-04-15 10:56:24 +0200

    Fixed exceptions

--- whatsgoingon/api/bandsintown.py
+++ whatsgoingon/api/bandsintown.py
@@ -16,7 +16,7 @@ app_id = "f614912873e83658d6de740a720d419c"
 # Helper stuff
 def get_args():
     if app_id is None:
-        raise InputException("app_id needs to be set!!")
+        raise Exception("app_id needs to be set!!")
     return [("format", "json"), ("app_id", app_id)]
 
 slashes_re = re.compile('\\\/')
@@ -30,7 +30,7 @@ def send_request(url, args =[]):
     g_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
     response = urllib.request.urlopen(base_url + "%s?%s" % (url, urllib.parse.urlencode(args + get_args())), context=g_context)
     if response.getcode() != 200:
-        raise RequestException("Request fail: %s" % response.read())
+        raise Exception("Request fail: %s" % response.read())
     if json.__name__ == "cjson":
         return json.decode(clean_slashes_for_cjson(response.read()))
     return json.loads(response.read())
